d3e9b7b925e11ba46b00cb757e11e3e87cd95068
Misha <svovoniks@gamil.com>
2023-02-10 21:30:21 +0300

    Full quadratic equation solution

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 def solve(a, b, c):
-    d = b**2 - 4*a*c
-    if d < 0:
-        print("no roots")
-    elif d == 0:
-        print(-b/(2*a))
+    if a == 0:
+        if b == 0:
+            if c == 0:
+                print("Anything will do")
+            else:
+                print("no roots")
+        else:
+            print(-b/(2/a))
     else:
-        print((-b-d**0.5)/(2*a), 
-              (-b+d**0.5)/(2*a))
+        d = b**2 - 4*a*c
+        if d < 0:
+            print("no roots")
+        elif d == 0:
+            print(-b/(2*a))
+        else:
+            print((-b-d**0.5)/(2*a), 
+                  (-b+d**0.5)/(2*a))
 
 
 print("input: a b c")
